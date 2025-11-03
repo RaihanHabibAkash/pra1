37fae2ab8aaefa627697ea15f7863605f0fd1500
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-11-03 03:16:51 -0800

    feat: add route for retrieving liked songs in songs API

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protectRoute, requireAdmin } from "../middlewere/auth.middlewere.js";
-import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getRecentlyPlayedSongs, getTrendingSongs } from "../controllers/song.controller.js";
+import { getAllSongs, getFeaturedSongs, getLikedSongs, getMadeForYouSongs, getRecentlyPlayedSongs, getTrendingSongs } from "../controllers/song.controller.js";
 
 const router = Router();
 
@@ -8,6 +8,7 @@ router.get("/", protectRoute, requireAdmin, getAllSongs);
 router.get("/fetured-songs", protectRoute, getFeaturedSongs);
 router.get("/made-for-you-songs", protectRoute, getMadeForYouSongs);
 router.get("/trending-songs", protectRoute, getTrendingSongs);
+router.get("/liked-songs", protectRoute, getLikedSongs);
 router.get("/recently-played-songs", protectRoute, getRecentlyPlayedSongs);
 
 export default router;
