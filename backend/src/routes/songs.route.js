fcd420a5ae0878331f0fb7ec78a458ee390c4e6d
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-11-04 05:42:48 -0800

    feat: add matchedLanguage route to songs API for improved song filtering

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protectRoute, requireAdmin } from "../middlewere/auth.middlewere.js";
-import { getAllSongs, getFeaturedSongs, getLikedSongs, getMadeForYouSongs, getRecentlyPlayedSongs, getTrendingSongs, matchedGenre } from "../controllers/song.controller.js";
+import { getAllSongs, getFeaturedSongs, getLikedSongs, getMadeForYouSongs, getRecentlyPlayedSongs, getTrendingSongs, matchedGenre, matchedLanguage } from "../controllers/song.controller.js";
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.get("/fetured-songs", protectRoute, getFeaturedSongs);
 router.get("/made-for-you-songs", protectRoute, getMadeForYouSongs);
 router.get("/trending-songs", protectRoute, getTrendingSongs);
 router.get("/fav-genre-songs", protectRoute, matchedGenre);
+router.get("/fav-language-songs", protectRoute, matchedLanguage);
 router.get("/liked-songs", protectRoute, getLikedSongs);
 router.get("/recently-played-songs", protectRoute, getRecentlyPlayedSongs);
 
