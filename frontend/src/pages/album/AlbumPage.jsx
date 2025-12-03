975d55573ac6e42cea86abeb3d6b672c94ba5eb6
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-12-02 23:13:18 -0800

    Album Page done for pc

--- frontend/src/pages/album/AlbumPage.jsx
+++ frontend/src/pages/album/AlbumPage.jsx
@@ -1,8 +1,17 @@
 
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { albumStore } from "@/stores/albumStore";
+import { Clock, Play } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
 
 
 const AlbumPage = () => {
@@ -33,6 +42,7 @@ const AlbumPage = () => {
   const gradientStyle = {
     background: `linear-gradient(
       to bottom,
+      
       rgb(${colors.r1}, ${colors.g1}, ${colors.b1}) 10%,
       rgb(${colors.r2}, ${colors.g2}, ${colors.b2}) 30%,
       rgb(0,0,0) 90%
@@ -56,10 +66,79 @@ const AlbumPage = () => {
               {/* Album Header */}
               <div className="flex p-4 gap-6 pb-8 justify-center">
                 <img src={currentAlbum?.imageUrl} alt={currentAlbum?.title}
-                className="size-40 shadow-lg object-cover rounded-full" />
-                
+                className="size-40 shadow-lg object-co
+                ver rounded-full" />
+                {/* Text things */}
+                <div className="flex flex-col justify-end">
+                  <p className="text-sm font-medium">
+                    Album
+                  </p>
+                  <h2 className="text-7xl font-bold my-4">
+                    { currentAlbum?.title }
+                  </h2>
+                  <div className="flex items-center gap-2 text-sm text-zinc-200">
+                    <span className="font-medium text-white">
+                      • { currentAlbum?.songs.length } songs
+                    </span>
+                    <span className="font-medium text-white">
+                      • { currentAlbum?.releaseYear }
+                    </span>
+                  </div>
+                </div>
               </div>
+              {/* Play Button */}
+                <div className="px-6 pb-4 flex items-center gap-6">
+                  <Button size="icon" className="size-14 rounded-full bg-green-500 hover:bg-green-400 
+                   hover:scale-110 hover:border-4 hover:border-black transition-all cursor-pointer">
+                    <Play className="size-7 text-black" />
+                  </Button>
+                </div>
 
+                {/* Table Section */}
+                <div className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 
+                text-sm text-zinc-400 border-b border-white/40">
+                  <div>#</div>
+                  <div>Title</div>
+                  <div>Released Date</div>
+                  <div>
+                    <Clock className="size-4" />
+                  </div>
+                </div>
+
+                {/* Songs */}
+                <div className="px-6">
+                  <div className="space-y-2 py-4">
+                    {currentAlbum?.songs.map((song, index) => (
+                      <div key={song._id} className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm
+                      text-zinc-400 hover:bg-white/40 hover:border-2 hover:border-black rounded-md group cursor-pointer">
+
+                        <div className="flex items-center justify-center">
+                          <span className="group-hover:hidden">{ index + 1 }</span>
+                          <Play className="h-4 w-4 hidden group-hover:block" />
+                        </div>
+
+                        <div className="flex items-center gap-3">
+                          <img src={song.imageUrl} alt={song.title} className="size-10" />
+                          <div>
+                            <div className="font-medium text-white">{song.title}</div>
+                              <div>{song.artist}</div>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center">
+                          { song.createdAt.split("T")[0] }
+                        </div>
+
+                        <div className="flex items-center">
+                          { formatTime(song.duration) }
+                        </div>
+
+
+
+                      </div>
+                    ))}
+                  </div>
+                </div>
             </div>
           </div>
         </ScrollArea>
