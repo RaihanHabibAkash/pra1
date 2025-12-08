98cd90bb7e82d4b7473d94c8f844e3c097f1e1a7
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-12-07 22:44:13 -0800

    authStore Created

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -1,1 +1,30 @@
-// Auth Store using zustand.
+import { axiosInstance } from "@/lib/axiosConnect";
+import { create } from "zustand";
+
+export const authStore = create(set => {
+    return {
+        isLoading: false,
+        error: null,
+        isAdmin: false,
+        chechAdmin: async () => {
+            set({ isLoading: true, error: null });
+
+            try {
+                const response = await axiosInstance.get("/admin");
+                set({ isAdmin: response.data.admin });
+            } catch (error) {
+                set({ isAdmin: false, error: error.response.data.message });
+            } finally {
+                set({ isLoading: false });
+            }
+        },
+        reset: () => {
+            set({ 
+                isAdmin: false, 
+                isLoading: false, 
+                error: null 
+            });
+        }
+
+    }
+})
